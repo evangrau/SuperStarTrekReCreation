6c0815d8279bc8482a9fff8f546684556ed3f8a0
Evan Grau <89275830+evangrau@users.noreply.github.com>
2023-09-29 08:45:11 -0500

    Squashed commit of the following:
    commit 9ff82e058605d27a793b5f14e3ac99a8538e9e0a
    Author: Evan Grau <89275830+evangrau@users.noreply.github.com>
    Date:   Fri Sep 29 08:00:18 2023 -0500
    
        reformatting
    
    commit f50c6121081d13a946988bc467d7eb72fb8de4c3
    Author: Evan Grau <89275830+evangrau@users.noreply.github.com>
    Date:   Thu Sep 28 10:53:03 2023 -0500
    
        incorrect class name
    
    commit da6cc077d9b5a4726ff17291c6ae59c7ec654dcd
    Author: Evan Grau <89275830+evangrau@users.noreply.github.com>
    Date:   Thu Sep 28 10:51:42 2023 -0500
    
        basic command line parsing from class example
    
    commit 660c3f96e3c41aa7daf11596f4ce08658576563f
    Author: Evan Grau <89275830+evangrau@users.noreply.github.com>
    Date:   Thu Sep 28 10:16:05 2023 -0500
    
        getting multiple command line arguments
    
    commit e43160f227764bf0ebf9606d6a54b9dcbb59f8c6
    Author: Evan Grau <89275830+evangrau@users.noreply.github.com>
    Date:   Tue Sep 26 16:59:08 2023 -0500
    
        moved comment

---
+++ CmdProc.java
@@ -0,0 +1,61 @@
+import java.util.NoSuchElementException;
+//import java.util.StringTokenizer;
+
+class CmdProc {
+    // private static StringTokenizer tokenizer = null;
+    private static StringSplitter splitter = null;
+
+    public static Token getToken() {
+        // if (tokenizer == null)
+        if (splitter == null) {
+            String cmdln = System.console().readLine();
+            // tokenizer = new StringTokenizer(cmdln, " ");
+            splitter = new StringSplitter(cmdln, " +");
+        }
+
+        // if (tokenizer.hasMoreTokens() == false)
+        if (splitter.hasMoreTokens() == false) {
+            // tokenizer = null;
+            splitter = null;
+
+            return new Token(TokenType.EOL, null, 0.0);
+        }
+
+        // String str = tokenizer.nextToken();
+        String str = splitter.nextToken();
+
+        try {
+            double dbl = Double.parseDouble(str);
+
+            return new Token(TokenType.DOUBLE, str, dbl);
+        } catch (NumberFormatException e) {
+            return new Token(TokenType.ALPHA, str, 0.0);
+        }
+    }
+
+    public static void flushTok() {
+        // tokenizer = null;
+        splitter = null;
+    }
+}
+
+class StringSplitter {
+    int index = -1;
+    private String[] split;
+
+    StringSplitter(String str, String regex) {
+        split = str.split(regex);
+        index = (split.length > 0) ? 0 : -1;
+    }
+
+    boolean hasMoreTokens() {
+        return (index > -1) && (index < split.length);
+    }
+
+    String nextToken() throws NoSuchElementException {
+        if (hasMoreTokens())
+            return split[index++];
+        else
+            throw new NoSuchElementException();
+    }
+}
