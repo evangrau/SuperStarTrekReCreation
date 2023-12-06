b330f75a3b6fbe0e3e844df14ce7a64bd33e9c27
Spindler1 <ecs19a@acu.edu>
2023-12-06 11:52:00 -0600

    Adding Impulse

--- src/main/java/Damages.java
+++ src/main/java/Damages.java
@@ -3,16 +3,17 @@ import java.util.Random;
 
 public class Damages {
     static boolean hasDamage = false;
-    static String[] devices = {"Computer", "D. S. Probe", "S. R. Sensors", "L. R. Sensors", "Subspace Radio"};
+    static String[] devices = { "Computer", "D. S. Probe", "S. R. Sensors", "L. R. Sensors", "Subspace Radio",
+            "Engine" };
+    static int[] deviceHealth = { 100, 100, 100, 100, 100, 100 };
 
     static void damageReport(Console con) {
         if (!hasDamage) {
             con.printf("All devices functional.\n\n");
             return;
-        }
-        else {
+        } else {
             con.printf("\nDEVICE        -REPAIR TIMES-\n");
-            con.printf("            IN FLIGHT   DOCKED\n"); 
+            con.printf("            IN FLIGHT   DOCKED\n");
             int rand = new Random().nextInt(devices.length);
             float flight = new Random().nextFloat() * 5;
             float docked = new Random().nextFloat() * 5;
@@ -23,5 +24,31 @@ public class Damages {
             }
             con.printf("  %s     %.2f     %.2f\n\n", devices[rand], flight, docked);
         }
+    }
+
+    static int hasDamage(String device) {
+        boolean inArray = false;
+        for (String element : devices) {
+            if (element == device) {
+                inArray = true;
+                break;
+            }
+        }
+        if (inArray) {
+            int ind = 0;
+            for (int i = 0; i < devices.length; i++) {
+                if (devices[i] == device)
+                    ind = i;
+            }
+
+            if (deviceHealth[ind] == 100)
+                return 0;
+            else
+                return 1;
+
+        }
+
+        else
+            return 2;
     }
 }
